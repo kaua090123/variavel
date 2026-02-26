8fe0d77f1382d117327314a1ba4842c399be38ac
kaua090123 <kaua.moretao.silva@escola.pr.gov.br>
2026-02-26 09:48:39 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,44 @@
+let a=40;
+let b=10;
+let c=20;
+let total=0;
+let totald=0;
+let x=5;
+total=(a+b+c);
+totald=(a+b+c)/x;
+console.log('O total é:',total);
+console.log('O total D é:',totald);
+console.log(outraconta());
+console.log(media());
+
+//inicio da Function
+function outraconta(){
+    var d=10;
+    var e=20;
+    var f=30;
+    var tot=0;
+    var totd=0;
+    var xx=100;
+    tot=(d+e+f);
+    totd=(d+e+f)/xx;
+    
+     //console.log('O tot é:',tot);
+    console.log('O totd é:',totd);
+    return tot;
+    return totd;
+    return toFixed
+}
+
+
+
+
+
+function media(){
+let nfinal=0;
+n1=30;
+n2=90;
+n3=100;
+nfinal=(n1+n2+n3)/3;
+nfinal.toFixed(2);
+console.log('A media final do aluno é:',nfinal);
+}
